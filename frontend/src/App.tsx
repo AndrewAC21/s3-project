5440cd297a409bb9a7f53d83f1a6d3c2baa3e197
Andres-Aguirre <oskr.ac73@gmail.com>
2022-11-28 23:02:08 -0500

    create form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+interface Photo {
+  title: string | undefined;
+  image: any;
 }
 
-export default App
+function App() {
+  const [photo, setPhoto] = useState<Photo>({
+    title: "",
+    image: null,
+  });
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log(e);
+    const formData = new FormData();
+    formData.append("photo", photo.image);
+
+    const response = await axios.post(
+      "http://localhost:3000/upload",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    console.log(response);
+  };
+  return (
+    <div className="App">
+      <div className="w-100vw h-100vh flex flex-col items-center">
+        <h1 className="mb-4">Sube tu foto a la nube!</h1>
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          className="flex flex-col items-center gap-2"
+        >
+          <input
+            type="file"
+            name="photo"
+            onChange={(e) => {
+              setPhoto({
+                title: e.target?.files?.[0].name,
+                image: e.target?.files?.[0],
+              });
+            }}
+          />
+          <button>send</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
